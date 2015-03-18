1eef10a3539bba3b797c46ad280d2c7b619f4b49
R_Paul <paulribbeck@live.de>
2015-03-18 15:50:46 +0000

    back #2

---
+++ koth.altis/config/blacklist.h
@@ -0,0 +1,20 @@
+class CfgRemoteExecCommands
+{
+	class allowDamage {};
+	class lock {};
+	class playMove {};
+	class playMoveNow {};
+	class say3d;
+	class setCaptive {};
+	class setFace {};
+	class setFlagTexture {};
+	class setObjectMaterial {};
+	class setObjectTextureGlobal {};
+	class setPos {};
+	class switchMove {};
+};
+
+class CfgRemoteExecFunctions
+{
+	class extdb_fnc_async { disabled = 1; };
+};
